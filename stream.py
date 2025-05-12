f1bcc9291cb25806aa6755fa50c93ef67a5d5496
Sandhiya <sandhiyaravin@gmail.com>
2025-05-12 15:11:10 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,21 +1,8 @@
-from flask import Flask, request, Response,jsonify, render_template, send_file
-import pandas as pd
-import joblib
-import os
-import io
-import pickle
-from io import BytesIO
 import streamlit as st
-import warnings
-
-model = joblib.load('./logistic_regression_model.pkl')
-st.write("Model loaded successfully!")
-
-import sklearn
 import sys
 
 st.write("Python version:", sys.version)
-st.write("scikit-learn version:", sklearn.__version__)
+
 
 if __name__ == '__main__':
     main()
